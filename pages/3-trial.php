5a49c8866fc370541b7c75c8642bba5c9a9a4edb
Marcel Montrey <MarcelMontrey@users.noreply.github.com>
2019-11-26 13:08:38 -0500

    Changed a few details with the stylesheet.

--- pages/3-trial.php
+++ pages/3-trial.php
@@ -4,10 +4,10 @@
 	<div class="content">
 		<div class="trial">
 			<div id="left-div">
-				<p><img id="left-img"></p>
+				<p><img id="left-img"></br></p>
 				<p><button id="left-btn" onclick="pullLever(0)">Pull lever</button></p>
 			</div>
-			<div id="training-txt"></div>
+			<div id="middle-div"></div>
 			<div id="right-div">
 				<p><img id="right-img"></p>
 				<p><button id="right-btn" onclick="pullLever(1)">Pull lever</button></p>
